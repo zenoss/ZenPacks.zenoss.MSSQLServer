e1328840c3f58368a236b4c4a16130e7280d52bd
pmcguire <pmcguire@f5df01ce-0320-0410-9ed9-ddcd13c732cc>
2012-03-26 14:39:26 +0000

    Fixes ZEN-214, change device class hierarchy for MSSQLSERVER zenpack, with migrate script; reviewed by epowell, da man
    git-svn-id: http://dev.zenoss.com/svnint/trunk/core/zenpacks/ZenPacks.zenoss.MSSQLServer@55800 f5df01ce-0320-0410-9ed9-ddcd13c732cc

---
+++ ZenPacks/zenoss/MSSQLServer/migrate/ChangeMonitoredTemplateToDeviceWMI.py
@@ -0,0 +1,41 @@
+######################################################################
+#
+# Copyright 2009 Zenoss, Inc.  All Rights Reserved.
+#
+######################################################################
+import Globals
+from Products.ZenModel.ZenPack import ZenPackMigration
+from Products.ZenModel.migrate.Migrate import Version
+
+import logging
+log = logging.getLogger("zen.ZenPacks.zenoss.MSSQLServer")
+
+class ChangeMonitoredTemplateToDeviceWMI(ZenPackMigration):
+    """
+    Change Monitoring template for MSSQLServer from Device (SNMP-oriented) to Device_WMI
+    """
+    version = Version(2, 0, 3)
+
+    def migrate(self, pack):
+        try:
+            devices = pack.dmd.getDmdRoot("Devices")
+            mssql_dc = devices.Server.Windows.WMI.MSSQLServer
+            templates = mssql_dc.zDeviceTemplates
+            changed = False
+            
+            if 'Device' in templates:
+                templates.remove('Device')
+                changed = True
+                
+            if 'Device_WMI' not in templates:
+                templates.insert(0, 'Device_WMI')
+                changed = True
+                
+            if changed:
+                mssql_dc.setZenProperty('zDeviceTemplates', templates)
+            
+        except Exception as e:
+            log.warn("Failed to modify zDeviceTemplates on MSSQLServer device class, %s", e)
+
+
+ChangeMonitoredTemplateToDeviceWMI()
